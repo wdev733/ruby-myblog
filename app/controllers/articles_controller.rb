ad9e54f9289c8ea81cc2ae9e4e4e9af5483d8c9e
wdev733 <wdev733@gmail.com>
2018-05-27 15:13:44 +0000

    Alpa blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @ar = Article.find(params[:id])
+    @ar.destroy
+    flash[:notes] = "Article was deleted..."
+    redirect_to articles_path
+  end
+  
   private
   
   def article_params
