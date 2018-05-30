4feb03dd114ac6da04fddd0a91bd5ed8eaeccc4b
wdev733 <wdev733@gmail.com>
2018-05-30 15:33:45 +0000

    complete articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   
   validates :title, presence: true, length: {minimum: 3, maximum:20}
-  validates :description, presence: true, length: {minimum: 10, maximum: 100}
+  validates :description, presence: true, length: {minimum: 10, maximum: 10000}
   
 end
