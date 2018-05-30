b962b21abdffb99d60ed5098b867b34f0ef83057
wdev733 <wdev733@gmail.com>
2018-05-30 23:00:34 +0000

    working on asos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
-  
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 3, maximum:20}
   validates :description, presence: true, length: {minimum: 10, maximum: 10000}
+  validates :user_id, presence: true
   
 end
