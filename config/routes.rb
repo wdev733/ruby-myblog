ce1e044dad581674958d1ef811f90ea2340fea42
wdev733 <wdev733@gmail.com>
2018-07-17 20:04:15 +0000

    Added new users part

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get "about", to: 'pages#about'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
