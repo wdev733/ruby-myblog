060d96313314e037f0ebd84a5afbf2138e004c9e
wdev733 <wdev733@gmail.com>
2018-07-18 11:24:08 +0000

    added show user and profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "your account has been updated successfully !"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   private
   
   def user_params
