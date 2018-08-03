891f254d20eab0e3387aafcf9ba7833c3093d0b7
wdev733 <wdev733@gmail.com>
2018-08-04 00:18:24 +0300

    deleted the table drop migrations for heroku porpuse

--- db/migrate/20180803183048_delete_categories.rb
+++
@@ -1,9 +0,0 @@
-class DeleteCategories < ActiveRecord::Migration
-  def up
-  	drop_table :categories
-  end
-
-  def down
-  	fail ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20180803194240_remove_article_categories.rb
+++
@@ -1,9 +0,0 @@
-class RemoveArticleCategories < ActiveRecord::Migration
-  def up	
-  	drop_table :article_categories
-  end
-
-  def down
-  	fail ActiveRecord::IrreversibleMigration
-  end
-end
