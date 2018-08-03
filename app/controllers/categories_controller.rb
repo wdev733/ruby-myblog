55567bb5a472c37252d7c90826daf868de7bdbea
wdev733 <wdev733@gmail.com>
2018-08-03 23:28:02 +0300

    edited categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,6 +25,21 @@ class CategoriesController < ApplicationController
 		@cat_arts = @category.articles.paginate(page: params[:page], per_page: 5)		
 	end
 
+	def edit
+		@category = Category.find(params[:id])
+	end
+
+	def update
+		@category = Category.find(params[:id])
+
+		if @category.update(category_params)
+			flash[:success] = "Category name was updated successfully"
+			redirect_to @category
+		else
+			render 'edit'
+		end
+	end
+
 
 	private
 
