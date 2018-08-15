30e796a67ea6bbf10d093fce72a7a78afd156f59
wdev733 <wdev733@gmail.com>
2018-08-15 21:03:56 +0300

    added device-width meta tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+// $(document).ready(function () {
+// 	$('#newcom').on('ajax:complete', function(evet, data, status){
+// 		$('#comments').html(data.responseText)
+// 	})
+// })
