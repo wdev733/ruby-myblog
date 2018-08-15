36f8562788b9069833964c13157e17a822e7fdaf
wdev733 <wdev733@gmail.com>
2018-08-15 21:24:53 +0300

    set the root to articles index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   
   
-  root 'pages#home'
+  root 'articles#index'
   get "about", to: 'pages#about'
   
   resources :articles do
